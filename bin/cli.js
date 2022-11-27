f90f3f7012b6fdaadd7f96cfd05ac883961ac41b
Maya Theresa Gopal <mayather@email.unc.edu>
2022-11-26 19:52:07 -0500

    Finished cli and rolls

--- bin/cli.js
+++ bin/cli.js
@@ -4,3 +4,11 @@
 import { roll } from "../lib/roll.js";
 import minimist from "minimist";
 
+const args = minimist(process.argv.slice(2));
+
+//defaults
+const sides = args.sides || 6;
+const dice = args.dice || 2; 
+const rolls = args.rolls || 1;
+
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
